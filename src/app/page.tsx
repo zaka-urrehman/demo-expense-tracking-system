2b212e6a7142a082a00bcea09e0ff7e5bd48058a
zaka2486 <114782819+zaka2486@users.noreply.github.com>
2023-06-04 02:26:04 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,158 @@
-import Image from 'next/image'
+'use client'
+import  { useState } from 'react';
+
+
+
+interface Transaction {
+  // id: string;
+  amount: number;
+  description: string;
+}
+
+enum TransactionType {
+  Expense = 'expense',
+  Income = 'income',
+}
+
+
 
 export default function Home() {
+
+  const [amount, setAmount] = useState('')
+  const [description, setDescription] = useState('')
+  const [transactionType, setTransactionType] = useState(TransactionType.Expense)
+  const [transactions,setTransactions]=useState<Transaction[]>([ ])
+
+  const addTransaction =()=>{
+    if(amount && description)
+      {
+        const newTransaction : Transaction={
+          amount : transactionType === TransactionType.Expense? -parseFloat(amount): parseFloat(amount),
+          description: description         
+        }
+        setTransactions([...transactions, newTransaction] );
+        setAmount('');
+        setDescription("");
+      }
+  }
+
+
+  const income=()=>{
+    const positiveTransactions = transactions.filter((transaction)=>transaction.amount > 0)
+    const totalIncome = positiveTransactions.reduce((total,transaction)=> total+transaction.amount,0)
+    return totalIncome
+  }
+
+  const expense=()=>{
+    const negativeTransactions = transactions.filter((transaction)=>transaction.amount<0)
+    const totalExpense = negativeTransactions.reduce((total,transaction)=>total+transaction.amount,0)
+    return totalExpense
+  }
+
+  const balance=()=>{
+
+  }
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex justify-center items-center">
+      <div className="max-w-5xl bg-cyan-200 py-10 px-10 ">
+        <h1 className="font-bold text-2xl text-center">Expense Tracking System</h1>
+
+        <div className="bg-gray-100 flex px-4 py-6 w-fit mx-auto m-2 rounded-xl">
+          <h3 className="font-bold">Available Balance:</h3>
+          <span className="ml-2">{income()+expense()}</span>
         </div>
+
+        <div className="flex gap-x-4">
+
+          <div className="  px-4 pt-4 pb-8 rounded-lg w-32 mx-auto bg-gradient-to-b from-[#68ff0026] to-[#41f565]">
+            <h3 className="font-bold text-[#00d800] text-center">Earnings:</h3>
+            <div className=" text-center text-white font-bold">{income()}</div>
+          </div>
+
+          <div className="  px-4 pt-4 pb-8 rounded-lg w-32 mx-auto bg-gradient-to-b from-[#c98a8a26] to-[#ed6262]">
+            <h3 className="font-bold text-[#dc5959] text-center">Spendings:</h3>
+            <div className=" text-center text-white font-bold">{expense()}</div>
+          </div>
+
+
+
+        </div>
+
+
+
+
+        <div className="bg-slate-200 m-4 p-4">
+          <h1 className="text-center font-semibold m-5">Transaction History</h1>
+
+          {transactions.map((trans)=>(
+            <div>
+                   <div className={`" my-1 flex justify-between p-2 rounded-lg shadow-lg " ${trans.amount>0?"bg-green-300":"bg-red-300"}`}>
+                   <h3>{trans.description}</h3>
+                   <h3>{trans.amount>0?'+':''}{trans.amount} </h3>
+                  </div>
+            </div>
+          ))}
+          
+        </div>
+
+
+
+        <div>
+          <h3 className="text-center font-bold text-2xl my-6">Add a new Transaction</h3>
+
+
+          <div>
+            <label className="m-2 font-semibold" >Select Transaction Type:</label>
+            <select 
+            name="Transaction Type" 
+            value={transactionType}
+            onChange={(e)=>setTransactionType(e.target.value as TransactionType)}
+            className="ml-2">
+              <option value={TransactionType.Expense}>Expense</option>
+              <option value={TransactionType.Income}>Income</option>
+            </select>
+          </div>
+
+          <div className="m-2 flex flex-col justify-center mt-6 ">
+            <label htmlFor="amount" className="font-semibold ml-2">Enter Amout</label>
+            <input 
+             type="number"
+             id='amount'
+             placeholder="Amount" 
+             value={amount}
+             onChange={(e)=>setAmount(e.target.value)}
+             className="px-2 py-2 rounded-3xl border-[5px] focus:border-gray-300 focus:scale-105" />
+          </div>
+
+          <div className="m-2 flex flex-col justify-center mt-6 ">
+            <label htmlFor="description" className="font-semibold ml-2">Enter Description</label>
+            <input 
+            type="text"
+            id='description' 
+            placeholder="Description" 
+            onChange={(e)=>setDescription(e.target.value)}
+            className="px-2 py-2 rounded-3xl border-[5px] focus:border-gray-300 focus:scale-105" />
+          </div>
+
+
+
+          <div className=" mt-6 flex justify-center">
+
+            <button 
+             type="submit"
+             className="bg-blue-400 text-white font-semibold rounded-xl px-8 py-2 text-center hover:scale-110 active:bg-blue-600"
+             onClick={addTransaction}
+             > Submit</button>
+
+          </div>
+
+
+
+        </div>
+
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
